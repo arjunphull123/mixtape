c1dc0e94250a45bacda4d5f416ce2fa3d130af33
arjunphull123 <arjunphull@arizona.edu>
2023-07-31 14:12:45 -0700

    fixed rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-export default {
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   base: '',
   build: {
-      target: 'esnext' //browsers can handle the latest ES features
+      target: 'esnext', //browsers can handle the latest ES features
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          privacy: resolve(__dirname, 'privacy.html'),
+        }
     }
   }
+})
