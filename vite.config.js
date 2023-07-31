2cfb4e9fa1827bd9d4146bbe916d92b3bbb9dfb9
arjunphull123 <arjunphull@arizona.edu>
2023-07-31 13:54:19 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,17 @@ export default {
   base: '',
   build: {
       target: 'esnext' //browsers can handle the latest ES features
+    },
+  pages: {
+    index: {
+      entry: './src/main.js',
+      template: './index.html',
+      title: 'mixtape'
+    },
+    privacy: {
+      entry: './src/main.js',
+      template: './privacy.html',
+      title: 'Privacy'
     }
+  }
   }
