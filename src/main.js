9eaf630437526e6cbef03e1e022f91e051d04dda
arjunphull123 <arjunphull@arizona.edu>
2023-08-04 13:34:00 -0700

    changed name to mixedify

--- src/main.js
+++ src/main.js
@@ -92,7 +92,7 @@ async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "https://mixtapeapp.netlify.app");
+    params.append("redirect_uri", "https://mixedify.netlify.app");
     params.append("scope", "user-top-read");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -126,7 +126,7 @@ async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "https://mixtapeapp.netlify.app");
+    params.append("redirect_uri", "https://mixedify.netlify.app");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
