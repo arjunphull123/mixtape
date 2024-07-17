039e093f169662a24eedaed69eec5b8e804b280f
arjunphull123 <arjunphull@arizona.edu>
2024-07-16 19:13:52 -0700

    Update helloWorld.js
    Logging id to console now

--- netlify/edge-functions/helloWorld.js
+++ netlify/edge-functions/helloWorld.js
@@ -1,3 +1,6 @@
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
 export default async (request, context) => {
     const url = new URL(request.url)
     
@@ -6,6 +9,22 @@ export default async (request, context) => {
     const page = await response.text()
 
     console.log("Hello, world")
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyBhHdbyP7hy2V4xlG9S_i9G62emf9mvIfI",
+        authDomain: "mixedify-40e2e.firebaseapp.com",
+        projectId: "mixedify-40e2e",
+        storageBucket: "mixedify-40e2e.appspot.com",
+        messagingSenderId: "1076825373232",
+        appId: "1:1076825373232:web:4c34f4277dd6f4bdc7cbf5"
+      };
+    
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+    console.log(id)
     
     return new Response(page, response);
 }
