7ad6785bef2e0f60f55ab4ca7f5ec98a73ec64ec
arjunphull123 <arjunphull@arizona.edu>
2024-07-21 13:30:03 -0700

    Trying the raw font data

--- sharpTest.js
+++ sharpTest.js
@@ -6,23 +6,21 @@ const color = "color-1"
 const titleSVG = `
     <svg width="1075" height="150">
         <style>
-            @font-face {
-                font-family: 'Ugly Dave';
-                src: url('/fonts/Ugly-Dave-Regular.woff2') format('woff2'),
-                url('/fonts/Ugly-Dave-Regular.woff') format('woff');
-                font-weight: normal;
-                font-style: normal;
-                font-display: swap;
-            }
-            .title {
-                fill: black;
-                font-size: 75px;
-                font-family: 'Ugly Dave';
-                font-weight: 600;
-                opacity: 77.5%;
-                letter-spacing: -.35%;
-            }
-        </style>
+              @font-face {
+                  font-family: 'Ugly Dave';
+                  font-weight: normal;
+                  font-style: normal;
+                  font-display: swap;
+              }
+              .title {
+                  fill: black;
+                  font-size: 75px;
+                  font-family: 'Ugly Dave';
+                  font-weight: 600;
+                  opacity: 77.5%;
+                  letter-spacing: -.35%;
+              }
+          </style>
         <text x="0" y="100" text-anchor="left" transform="rotate(-0.75)" class="title">${title}</text>
     </svg>
 `
@@ -39,7 +37,7 @@ const titleSVG = `
 const titleBuffer = Buffer.from(titleSVG)
 
 
-await sharp(`${color}-base.png`)
+await sharp(`public/${color}-base.png`)
     .composite([{
         input: titleBuffer,
         top: 185,
